aca2098480a0e48c90bd5487abcace3857898643
Argerie <argerie.m08@gmail.com>
2025-10-07 19:20:06 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Argerie.github.io',
+  site: 'https://Argerie.github.io'})
 
   vite: {
       plugins: [tailwindcss()],
